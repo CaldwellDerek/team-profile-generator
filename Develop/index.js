437daca3a82e9084c76e50164c66ac308084d2fd
Derek Caldwell <djohncaldwell@gmail.com>
2023-01-26 17:18:52 -0800

    created add an engineer functionality, made displayMenu recursive

--- Develop/index.js
+++ Develop/index.js
@@ -6,17 +6,69 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const teamArray = [];
 
-// const newManager = new Manager("manager", 2, "manager@email.com", 13);
-// const newEngineer = new Engineer("engineer", 3, "engineer@email.com", "engineer@github.com");
-// const newIntern = new Intern("intern", 4, "intern@email.com", "skewl");
-
-// const testArray = [newManager, newEngineer, newIntern];
-
 // fs.writeFile("./Output/index.html", generateHTML(testArray), (error) => {
 //     console.log(error);
 // });
 
+ // Begins the prompt to add an Engineer, Intern or finish application
+const displayMenu = async () => {
+    const menu = await i.prompt([
+        {
+            type: "list",
+            name: "menu",
+            message: "Finish creating your team:",
+            choices: ["- Add an Engineer", "- Add an Intern", "- Finish Team Building"]
+        }
+    ])
+
+    // Checks if the user selected Add an Engineer from the menu prompt
+    if (menu.menu === "- Add an Engineer"){
+        // Prompt to gather engineer's information
+        const newE = await i.prompt([
+            {
+                type: "input",
+                name: "engName",
+                message: "Enter Engineer's name:"
+            },
+            {
+                type: "input",
+                name: "engID",
+                message: "Enter Engineer's employee ID:"
+            },
+            {
+                type: "input",
+                name: "engEmail",
+                message: "Enter Engineer's email:"
+            },
+            {
+                type: "input",
+                name: "gitHub",
+                message: "Enter the Engineer's GitHub Username:"
+            }
+        ]);
+
+        teamArray.push(new Engineer(newE.engName, newE.engID, newE.engEmail, newE.gitHub));
+
+        console.log(teamArray);
+
+        displayMenu();
+
+    }
+
+    // Checks if the user selected Add an Intern from the menu prompt
+    if (menu.menu === "- Add an Intern"){
+        console.log("adding an intern!");
+    }
+
+    // Checks if the user selected Finish Team Building from the menu prompt
+    if (menu.menu === "- Finish Team Building"){
+        console.log("I am finished team building!");
+    }
+    
+}
+
 const startProgram = async () => {
+
     // Initial prompt to create manager on application start
     const init = await i.prompt([
         {
@@ -43,31 +95,7 @@ const startProgram = async () => {
     // Takes values from inital propmpt to create a new Manager and append it to the teamArray
     teamArray.push(new Manager(init.managerName, init.managerID, init.managerEmail, init.officeNumber));
 
-    // Begins the prompt to add an Engineer, Intern or finish application
-    const menu = await i.prompt([
-        {
-            type: "list",
-            name: "menu",
-            message: "Finish creating your team:",
-            choices: ["- Add an Engineer", "- Add an Intern", "- Finish Team Building"]
-        }
-    ])
-
-    // Checks if the user selected Add an Engineer from the menu prompt
-    if (menu.menu === "- Add an Engineer"){
-        console.log("adding an engineer!");
-    }
-
-    // Checks if the user selected Add an Intern from the menu prompt
-    if (menu.menu === "- Add an Intern"){
-        console.log("adding an intern!");
-    }
-
-    // Checks if the user selected Finish Team Building from the menu prompt
-    if (menu.menu === "- Finish Team Building"){
-        console.log("I am finished team building!");
-    }
-
+    displayMenu();
 }
 
 startProgram();
